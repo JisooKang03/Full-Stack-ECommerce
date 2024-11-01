c2f0d8e5f7658a7827ed00cf52e004a68cc51fae
Ji-Soo Kang <kangjisoo250@gmail.com>
2024-11-01 18:31:09 -0400

    delete

--- frontend/lib/sanity.js
+++
@@ -1,8 +0,0 @@
-// frontend/lib/sanity.js
-import sanityClient from '@sanity/client';
-
-export const client = sanityClient({
-  projectId: 'your_project_id',
-  dataset: 'production',
-  useCdn: true,
-});
--- frontend/lib/stripe.js
+++
@@ -1,4 +0,0 @@
-// frontend/lib/stripe.js
-import { loadStripe } from '@stripe/stripe-js';
-
-export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
